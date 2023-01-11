d9a8b549e8d28c1c2bfccb7e9d7bce21b6355b92
Emre idi <emreidi@hotmail.com>
2023-01-11 15:26:01 +0300

    18. gunsonu

--- src/test/java/tests/day14_testNGFramework/C03_NutellaTesti.java
+++ src/test/java/tests/day14_testNGFramework/C03_NutellaTesti.java
@@ -32,5 +32,6 @@ public class C03_NutellaTesti {
 
 
 
+
     }
 }
---
+++ src/test/java/tests/day18_htmlReports/C01_HTML_Reports.java
@@ -0,0 +1,35 @@
+package tests.day18_htmlReports;
+
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.TestBaseRapor;
+
+import java.security.Key;
+
+public class C01_HTML_Reports extends TestBaseRapor {
+
+    @Test
+    public void test01(){
+
+        extentTest=extentReports.createTest("Nutella testi","kullanici amazonda nutella arayabilmeli");
+
+        // amazona gidelim
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        extentTest.info("Amazon anasayfaya gidildi");
+        // nutella aratalim
+        AmazonPage amazonPage = new AmazonPage();
+        amazonPage.amazonAramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
+        extentTest.info("Arama kutusuna nutella yazip arama yapildi");
+        // sonuclarin nutella icerdigini test edelim
+        String actualSonucYazisi=amazonPage.aramaSonucuElementi.getText();
+        String expectedSonucYazisi="Nutella";
+        extentTest.info("Arama sonuclari kaydedildi");
+        Assert.assertTrue(actualSonucYazisi.contains(expectedSonucYazisi));
+        extentTest.pass("Arama sonuclarinin Nutella icerdigi test edildi");
+
+    }
+}
